aa085b0f61cee695df16ba7724304b28a4bdb438
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-25 22:42:51 +0200

    wip

--- app.js
+++ app.js
@@ -1,56 +1,73 @@
 const sgMail = require('@sendgrid/mail');
-const { body } = require('./body');
+const { smsBody } = require('./body');
 
 require("dotenv").config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const client = require("twilio")(process.env.TEST_SID, process.env.TEST_TOKEN);
+const client = require("twilio")(process.env.SID, process.env.AUTH_TOKEN);
 
-const from = process.env.TEST_FROM.toString();
+// steps: Define .env and fill with credentials, define date, define body in variable or create file - many options to choose 
+
+// Create .env file and fill with necessary credentials for sendgrid (optional) and twilio
+const from = process.env.FROM_NUMBER.toString();
 const to = process.env.DANAR.toString();
 
 const mailFrom = process.env.MAIL_FROM;
 const mailTo = process.env.MAIL_TO;
+//
 
-const d = new Date("June 25, 2020 21:36:50");
+// Define date and time of when to send the sms
+const d = new Date("June 25, 2020 22:24:00");
+//
+
 const mill = d.getTime() - (new Date()).getTime();
 
-console.log(body);
+// Please define body here if the smsBody file does not exist
+const body = "was geht mo, sms " 
+//smsBody
+//
+
+
 
 const run = () => {
-    // setTimeout(() => {
-    // }, mill);
-    
-    client.messages
-      .create({
-        body,
-        from,
-        to,
-      })
-      .then((message) => {
-        console.log("SMS------------------------------------------>");
-        console.log(message);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-      
-      const msg = {
-        to: mailTo,
-        from: mailFrom,
-        subject: 'SMS was sent successfully',
-        text: `Hi Zubeir,\n\n The SMS with the message: \n\n"${body}" \n\n was sent succesfully to the number ${to} `,
-      };
-      sgMail.send(msg).then( res => {
-          console.log("EMAIL-------------------------------------->");
-          console.log(res);
-      }, error => {
-          if(error){
-              console.error(error);
-          }
-      })
+    if(body) {
+        // setTimeout(() => {
+        // }, mill);
+        client.messages
+          .create({
+            body,
+            from,
+            to,
+          })
+          .then((message) => {
+            console.log("SMS------------------------------------------>");
+            console.log(message);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+          
+          const msg = {
+            to: mailTo,
+            from: mailFrom,
+            subject: 'SMS was sent successfully',
+            text: `Hi Zubeir,\n\n The SMS with the message: \n\n"${body}" \n\n was sent succesfully to the number ${to} `,
+          };
+          // sgMail.send(msg).then( res => {
+          //     console.log("EMAIL-------------------------------------->");
+          //     console.log(res);
+          // }, error => {
+          //     if(error){
+          //         console.error(error);
+          //     }
+          // })
+        
+    } else {
+        console.error("Please define body");
+    }
+
 }
 
-// run();
+run();
 
 
