cc9d7d809ce7e9f48b902afdb6efe7cf85a7a373
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-25 22:06:07 +0200

    use sendgrid and add body

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
-const { LogContext } = require('twilio/lib/rest/serverless/v1/service/environment/log');
+const sgMail = require('@sendgrid/mail');
+const { body } = require('./body');
 
-require('dotenv').config();
+require("dotenv").config();
 
-const client = require('twilio')(process.env.TEST_SID, process.env.TEST_TOKEN);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const client = require("twilio")(process.env.TEST_SID, process.env.TEST_TOKEN);
 
 const from = process.env.TEST_FROM.toString();
 const to = process.env.DANAR.toString();
 
+const mailFrom = process.env.MAIL_FROM;
+const mailTo = process.env.MAIL_TO;
 
-client.messages.create({
-    body: "Sent from a Twilio Trial Account",
-    from,
-    to
-}).then(message => {
-    console.log(message);
-}).catch(err => {
-    console.error(err);
-})
+const d = new Date("June 25, 2020 21:36:50");
+const mill = d.getTime() - (new Date()).getTime();
+
+console.log(body);
+
+const run = () => {
+    // setTimeout(() => {
+    // }, mill);
+    
+    client.messages
+      .create({
+        body,
+        from,
+        to,
+      })
+      .then((message) => {
+        console.log("SMS------------------------------------------>");
+        console.log(message);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+      
+      const msg = {
+        to: mailTo,
+        from: mailFrom,
+        subject: 'SMS was sent successfully',
+        text: `Hi Zubeir,\n\n The SMS with the message: \n\n"${body}" \n\n was sent succesfully to the number ${to} `,
+      };
+      sgMail.send(msg).then( res => {
+          console.log("EMAIL-------------------------------------->");
+          console.log(res);
+      }, error => {
+          if(error){
+              console.error(error);
+          }
+      })
+}
+
+// run();
+
+
